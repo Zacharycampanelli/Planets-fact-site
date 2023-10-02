998658af07e0207ae4b51fd014c58758f983e84c
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2023-10-02 16:05:03 -0400

    mobile navbar layout finished

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,22 +3,35 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 import planetsData from '../assets/data/data';
+// import  chevron from '../assets/images/icon-chevron.svg';
 
 function NavBar() {
+  const createDot = (planet) => {
+    return `dot-${planet}`
+  }
+
+
   return (
-    <Navbar expand="md" className="bg-body-tertiary">
-      <Container>
+    <Navbar expand="md"  id="navbar">
+      <Container id="navbar-container">
         <Navbar.Brand id="title">The Planets</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav"> <img src={'/src/assets/images/icon-hamburger.svg'} alt="hamburger dropdown menu" className='hamburger-icon' /></Navbar.Toggle>
+        <hr id="nav-border" /> 
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <ul>
+            <ul className="planet-list">
               {planetsData.map((planet, index) => (
-                <li key={index}>
-                  {/* <Nav.Link> */}
-                  <Link to={`/planet/${planet.name.toLowerCase()}`}>{planet.name}</Link>
-                  {/* </Nav.Link> */}
-                </li>
+         
+                <Nav.Item className='planet-list-item' key={index}>
+                  <span>
+
+                  <div id={createDot(planet.name)} ></div>
+                  <Link className='planet-list-item-link' to={`/planet/${planet.name.toLowerCase()}`}>{planet.name}</Link>
+                
+                  </span>
+                   <img src={'/src/assets/images/icon-chevron.svg'} alt="chevron arrow" className='planet-list-item-arrow' />
+               </Nav.Item> 
+          
               ))}
             </ul>
           </Nav>
--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -2,10 +2,12 @@ import NavBar from "../Components/NavBar"
 
 const Layout = ({children}) => {
   return (
-    <>
+    <div id="layout"> 
       <NavBar />
+     
+
       <main>{children}</main>
-    </>
+    </div>
   )
 }
 
