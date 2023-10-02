411a1f24f84fa6262554da636032f2c7223235e5
Zachary Campanelli <84996750+Zacharycampanelli@users.noreply.github.com>
2023-10-02 19:49:13 -0400

    tablet size navbar finished

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,39 +3,49 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
 import planetsData from '../assets/data/data';
-// import  chevron from '../assets/images/icon-chevron.svg';
+import { useMediaQuery } from '@uidotdev/usehooks';
 
 function NavBar() {
-  const createDot = (planet) => {
-    return `dot-${planet}`
-  }
+  const isLargerThanMedium = useMediaQuery('only screen and (min-width: 768px)');
 
+  const createDot = (planet) => {
+    return `dot-${planet}`;
+  };
+
+  // if (size.width > 480) return <>hi</>;
 
   return (
-    <Navbar expand="md"  id="navbar">
+    <Navbar expand="lg" id="navbar">
       <Container id="navbar-container">
         <Navbar.Brand id="title">The Planets</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav"> <img src={'/src/assets/images/icon-hamburger.svg'} alt="hamburger dropdown menu" className='hamburger-icon' /></Navbar.Toggle>
-        <hr id="nav-border" /> 
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          {' '}
+          <img src={'/src/assets/images/icon-hamburger.svg'} alt="hamburger dropdown menu" className="hamburger-icon" />
+        </Navbar.Toggle>
+        {!isLargerThanMedium && <hr id="nav-border" />}
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <ul className="planet-list">
+            <div className="planet-list">
               {planetsData.map((planet, index) => (
-         
-                <Nav.Item className='planet-list-item' key={index}>
+                <Nav.Item className="planet-list-item" key={index}>
                   <span>
-
-                  <div id={createDot(planet.name)} ></div>
-                  <Link className='planet-list-item-link' to={`/planet/${planet.name.toLowerCase()}`}>{planet.name}</Link>
-                
+                    {!isLargerThanMedium && <div id={createDot(planet.name)}></div>}
+                    <Link className="planet-list-item-link" to={`/planet/${planet.name.toLowerCase()}`}>
+                      {planet.name}
+                    </Link>
                   </span>
-                   <img src={'/src/assets/images/icon-chevron.svg'} alt="chevron arrow" className='planet-list-item-arrow' />
-               </Nav.Item> 
-          
+                  {!isLargerThanMedium && <img
+                    src={'/src/assets/images/icon-chevron.svg'}
+                    alt="chevron arrow"
+                    className="planet-list-item-arrow"
+                  />}
+
+                </Nav.Item>
               ))}
-            </ul>
+            </div>
           </Nav>
         </Navbar.Collapse>
+              {isLargerThanMedium && <hr id="nav-border" />}
       </Container>
     </Navbar>
   );
